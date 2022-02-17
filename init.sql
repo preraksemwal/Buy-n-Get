375158b84d7b0b1b2a6461ccd5925cac190df22d
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-02-18 00:06:06 +0530

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,32 @@
+create database test;
+use test;
+
+
+
+create table accounts(
+	username varchar(50) unique primary key,
+    password varchar(50),
+    email varchar(50),
+    customer_id int
+);
+
+create table customer(
+	customer_id int primary key,
+    customer_name varchar(100),
+    age int,
+    gender varchar(20),
+    phone_no varchar(15),
+    country varchar(30),
+    state varchar(60),
+    street_name varchar(50),
+    street_no int,
+    pincode int
+);
+
+create table orders(
+	order_id int primary key,
+    item_id int,
+    quantity int,
+    customer_id int,
+    foreign key(customer_id) references customers
+);
