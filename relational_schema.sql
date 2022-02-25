b877133b2091cbcca8ff5e454f02b32a7f07d80e
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-02-25 11:11:52 +0530

    Update relational_schema.sql

--- relational_schema.sql
+++ relational_schema.sql
@@ -1,30 +1,48 @@
 
-create table accounts (
-	username VARCHAR(50),
-	email VARCHAR(50),
-	password VARCHAR(50),
-	customer_id INT
+CREATE TABLE accounts (
+	username VARCHAR(50)  PRIMARY KEY,
+	email VARCHAR(50) UNIQUE CHECK(POSITION("@" IN email) != 0),
+	password VARCHAR(50) CHECK(LENGTH(password) >= 6),
+	customer_id INT UNIQUE AUTO_INCREMENT
 );
 
-create table customers (
-	customer_id INT,
-	customer_name VARCHAR(50),
-	age INT,
+CREATE TABLE customers (
+	customer_id INT PRIMARY KEY AUTO_INCREMENT,
+	customer_name VARCHAR(50) NOT NULL,
+	age INT CHECK(age >= 16),
 	gender VARCHAR(50),
-	phone VARCHAR(50),
-	country VARCHAR(50),
-	state VARCHAR(50),
-	street_name VARCHAR(50),
-	street_no VARCHAR(50),
-	pincode VARCHAR(50)
+	phone VARCHAR(50) NOT NULL, 
+	country VARCHAR(50) NOT NULL,
+	state VARCHAR(50) NOT NULL,
+	street_name VARCHAR(50) NOT NULL,
+	street_no INT NOT NULL,
+	pincode INT NOT NULL CHECK
 );
 
-create table orders(
-	order_id int primary key,
-    item_id int,
-    quantity int,
-    customer_id int,
-    foreign key(customer_id) references customers(customer_id)
+
+CREATE TABLE buyers(
+	customer_id INT PRIMARY KEY,
+	cart_id INT NOT NULL,
+	FOREIGN KEY(cart_id) REFERENCES carts(cart_id)
 );
 
+
+CREATE TABLE carts(
+	cart_id INT PRIMARY KEY AUTO_INCREMENT, 
+	customer_id INT UNIQUE NOT NULL,
+	items ,
+	FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+);
+
+
+
+CREATE TABLE orders(
+	order_id INT PRIMARY KEY AUTO_INCREMENT,
+    item_id INT NOT NULL,
+    customer_id INT NOT NULL,
+    quantity INT CHECK(quantity > 0),
+    order_date DATE DEFAULT SYSDATE(),
+    FOREIGN KEY(customer_id) REFERENCES customers(customer_id),
+    FOREIGN KEY(item_id) REFERENCES items(item_id)
+);
 
