6ee2ea217b8e60a7400c32da61da128606a19699
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-02-28 11:53:09 +0530

    Create populate_owners.sql

---
+++ Data/populate_owners.sql
@@ -0,0 +1,4 @@
+insert into owners values(1, 'Vineet Kaul', 'vineet20153@iiitd.ac.in', 'sdfs$#etjdg1R', '9876543222');
+insert into owners values(2, 'Prerak Semwal', 'prerak20105@iiitd.ac.in', '@edve$%#Dgsgsd', '1234567899');
+insert into owners values(3, 'Janhavi Bayanwar', 'bayanwar20043@iiitd.ac.in', '^yrHa!643DFGsdf', '1231231235');
+insert into owners values(4, 'Abhinav Kumar Sinha', 'abhinav20012@iiitd.ac.in', 'werwr$%#@46422', '4545767691');	
