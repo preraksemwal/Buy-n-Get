8f79a4c3f9ebdd757f961b07c8de3eba831a8b0c
The Punisher <88607607+VineetVK09@users.noreply.github.com>
2022-02-28 14:50:42 +0530

    Update populate_support.sql

--- Data/populate_support.sql
+++ Data/populate_support.sql
@@ -1,4 +1,4 @@
-insert into support (support_id, customer_id, issue) values(1, 50, 'the colour of phone does not matches the one i ordered');
-insert into support (customer_id, issue) values( 3, 'i am not able to add the products to my cart');
+insert into support (support_id, customer_id, issue) values(1, 269, 'the colour of phone does not matches the one i ordered');
+insert into support (customer_id, issue) values( 450, 'i am not able to add the products to my cart');
 insert into support (customer_id, issue) values( 73, 'the iron i ordered is not getting heatup properly');
 insert into support (customer_id, issue) values( 90, 'the lid of the blender was broken');
