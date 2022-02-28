5a22fb484a60043e52f8516d803826d1e9179b96
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-02-28 12:44:00 +0530

    Update relational_schema.sql

--- relational_schema.sql
+++ relational_schema.sql
@@ -98,7 +98,7 @@ CREATE TABLE payments(
 
 CREATE TABLE transactions(
 	transaction_id INT PRIMARY KEY AUTO_INCREMENT,
-	order_id INT NOT NULL,
+	order_id INT UNIQUE NOT NULL,
 	customer_id INT NOT NULL,
 	mode VARCHAR(20) NOT NULL,
 	amount INT NOT NULL		
