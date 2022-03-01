525b579ca9771b96bca96d1a952fb2e8e51859fa
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-03-01 20:51:47 +0530

    Create populate_payments.sql

---
+++ Data/populate_payments.sql
@@ -0,0 +1,6 @@
+insert into payments(payment_id, customer_id, mode, amount) values (1, 2, 'NetBanking', 100);
+
+insert into payments(customer_id, mode, amount) values (3, 'UPI', 70);
+insert into payments(customer_id, mode, amount) values (5, 'UPI', 140);
+insert into payments(customer_id, mode, amount) values (2, 'UPI', 500);
+insert into payments(customer_id, mode, amount) values (6, 'Credit-Card', 212);
