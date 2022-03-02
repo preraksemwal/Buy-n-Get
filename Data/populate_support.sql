260a7ba6c44a98d9b22d73b3de4f121941126933
abhinav20012 <88596613+abhinav20012@users.noreply.github.com>
2022-03-02 20:14:16 +0530

    Update populate_support.sql

--- Data/populate_support.sql
+++ Data/populate_support.sql
@@ -1,4 +1,4 @@
-insert into support (support_id, customer_id, issue) values(1, 269, 'Ihe colour of phone does not match with the one I ordered', '2021-10-10');
+insert into support (support_id, customer_id, issue,issue_date) values(1, 269, 'Ihe colour of phone does not match with the one I ordered', '2021-10-10');
 insert into support (customer_id, issue, issue_date) values( 450, 'I am not able to add products to my cart', '2022-11-12');
 insert into support (customer_id, issue, issue_date) values( 73, 'The iron I ordered is not heating up properly', '2022-01-27');
 insert into support (customer_id, issue, issue_date) values( 90, 'The lid of the blender was broken', '2022-02-19');
