830a93a98c322cf44b2dec55edc06d2b4ed37135
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-04-08 17:05:41 +0530

    Update relational_schema.sql

--- relational_schema.sql
+++ relational_schema.sql
@@ -11,11 +11,11 @@ CREATE TABLE accounts (
 	username VARCHAR(50) PRIMARY KEY,
 	email VARCHAR(50) NOT NULL UNIQUE CHECK(POSITION("@" IN email) != 0),
 	password VARCHAR(50) NOT NULL CHECK(LENGTH(password) >= 6),
-	customer_id INT UNIQUE AUTO_INCREMENT
+	customer_id INT UNIQUE NOT NULL AUTO_INCREMENT
 );
 
 CREATE TABLE customers (
-	customer_id INT PRIMARY KEY AUTO_INCREMENT,
+	customer_id INT PRIMARY KEY,
 	customer_name VARCHAR(50) NOT NULL,
 	age INT CHECK(age >= 16),
 	gender VARCHAR(50),
@@ -24,37 +24,31 @@ CREATE TABLE customers (
 	state VARCHAR(50) NOT NULL,
 	street_name VARCHAR(50) NOT NULL,
 	street_no INT NOT NULL,
-	pincode VARCHAR(5) NOT NULL
+	pincode VARCHAR(5) NOT NULL,
+	FOREIGN KEY(customer_id) REFERENCES accounts(customer_id)
+);
+
+
+
+CREATE TABLE buyers(
+	customer_id INT PRIMARY KEY,
+    FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
 );
 
 CREATE TABLE carts(
 	cart_id INT PRIMARY KEY AUTO_INCREMENT, 
 	customer_id INT UNIQUE NOT NULL,
-	items_id varchar(16000) DEFAULT NULL            
+	FOREIGN KEY(customer_id) REFERENCES buyers(customer_id)        
 );
 
-CREATE TABLE buyers(
-	customer_id INT PRIMARY KEY,
-	cart_id INT NOT NULL,
-	FOREIGN KEY(cart_id) REFERENCES carts(cart_id),
-    	FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+CREATE TABLE stores(
+	cart_id INT NOT NULL, 
+	item_id INT NOT NULL,
+	quantity INT NOT NULL,
+	FOREIGN KEY(cart_id) REFERENCES carts(cart_id)        
 );
 
-CREATE TABLE orders(
-	order_id INT PRIMARY KEY AUTO_INCREMENT,
-    	item_id INT NOT NULL,
-    	customer_id INT NOT NULL,
-    	quantity INT CHECK(quantity > 0),
-    	order_date DATE NOT NULL,
-    	delivery_date DATE NOT NULL,
-    	FOREIGN KEY(customer_id) REFERENCES buyers(customer_id)
-);
 
-CREATE TABLE sellers(
-	customer_id INT PRIMARY KEY,
-	items_id VARCHAR(12000) NOT NULL,
-    	FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
-);
 
 CREATE TABLE items(
 	item_id INT PRIMARY KEY AUTO_INCREMENT,
@@ -65,29 +59,67 @@ CREATE TABLE items(
 	selling_price FLOAT NOT NULL CHECK(selling_price > 0)
 );
 
+
+CREATE TABLE orders(
+	order_id INT PRIMARY KEY AUTO_INCREMENT,
+	customer_id INT NOT NULL,
+    order_date DATE NOT NULL,
+    delivery_date DATE NOT NULL,
+    FOREIGN KEY(customer_id) REFERENCES buyers(customer_id)
+);
+
+CREATE TABLE ordered_items(
+	order_id INT NOT NULL,
+	item_id INT NOT NULL,
+    quantity INT CHECK(quantity > 0),
+    FOREIGN KEY(order_id) REFERENCES orders(order_id),
+    FOREIGN KEY(item_id) REFERENCES items(item_id)
+);
+
+
+
+
+CREATE TABLE sellers(
+	customer_id INT PRIMARY KEY,
+	FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+);
+
+CREATE TABLE sells(
+	customer_id INT NOT NULL,
+	item_id INT NOT NULL,
+	FOREIGN KEY(customer_id) REFERENCES sellers(customer_id),
+    FOREIGN KEY(item_id) REFERENCES items(item_id)
+);
+
+
+
 CREATE TABLE feedback(
 	customer_id INT PRIMARY KEY,
-	rating INT CHECK(rating BETWEEN 1 AND 5)
+	rating INT CHECK(rating BETWEEN 1 AND 5),
+	FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
 );
 
 CREATE TABLE support(
 	support_id INT PRIMARY KEY AUTO_INCREMENT,
 	customer_id INT,
 	issue VARCHAR(1000),
-	issue_date date
+	issue_date date,
+	FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
 );
 
 CREATE TABLE payments(
 	payment_id INT PRIMARY KEY AUTO_INCREMENT,
 	customer_id INT NOT NULL,
 	mode VARCHAR(20) NOT NULL,
-	amount INT NOT NULL	
+	amount INT NOT NULL	,
+	FOREIGN KEY(customer_id) REFERENCES sellers(customer_id)
 );
 
 CREATE TABLE transactions(
 	order_id INT PRIMARY KEY,
 	customer_id INT NOT NULL,
 	mode VARCHAR(20) NOT NULL,
-	amount INT NOT NULL		
+	amount INT NOT NULL,
+	FOREIGN KEY(customer_id) REFERENCES buyers(customer_id),
+	FOREIGN KEY(order_id) REFERENCES orders(order_id)
 );
-
