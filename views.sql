1e0e5be23caa7c65cbc13bda4bbdae58ce5903aa
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-04-23 17:48:23 +0530

    Update views.sql

--- views.sql
+++ views.sql
@@ -1,1 +1,13 @@
+CREATE VIEW owners_info AS
+SELECT owner_name AS Name, email 
+FROM owners;
 
+
+CREATE VIEW accounts_info AS
+SELECT username, email 
+FROM accounts;
+
+
+CREATE VIEW customers_info AS 
+SELECT customer_name, gender, phone_no, country, state, street_name, street_no, pincode
+FROM customers;
