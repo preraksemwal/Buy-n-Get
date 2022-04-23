939e46e65ac2714298fdca72d3adbca8a5157744
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-04-23 17:47:55 +0530

    Update grants.sql

--- grants.sql
+++ grants.sql
@@ -1,1 +1,66 @@
+CREATE USER owner1@localhost IDENTIFIED by 'Abhinav';
+GRANT SELECT ON owners_info to owner1@localhost;
+GRANT SELECT ON customers to owner1@localhost;
+GRANT DELETE ON accounts to owner1@localhost;
+GRANT SELECT ON accounts_info to owner1@localhost;
+GRANT SELECT ON sellers to owner1@localhost;
+GRANT SELECT ON buyers to owner1@localhost;
+GRANT ALL ON items to owner1@localhost;
+GRANT SELECT ON orders to owner1@localhost;
+GRANT SELECT ON ordered_items to owner1@localhost;
+GRANT SELECT ON support to owner1@localhost;
+GRANT SELECT, DELETE ON feedback to owner1@localhost;
+GRANT SELECT ON payments to owner1@localhost;
+GRANT SELECT ON transactions to owner1@localhost;
+GRANT SELECT ON sells to owner1@localhost;
 
+
+CREATE USER owner2@localhost IDENTIFIED by 'Janhavi';
+GRANT SELECT ON owners_info to owner1@localhost;
+GRANT SELECT ON customers to owner1@localhost;
+GRANT DELETE ON accounts to owner1@localhost;
+GRANT SELECT ON accounts_info to owner1@localhost;
+GRANT SELECT ON sellers to owner1@localhost;
+GRANT SELECT ON buyers to owner1@localhost;
+GRANT ALL ON items to owner1@localhost;
+GRANT SELECT ON orders to owner1@localhost;
+GRANT SELECT ON ordered_items to owner1@localhost;
+GRANT SELECT ON support to owner1@localhost;
+GRANT SELECT, DELETE ON feedback to owner1@localhost;
+GRANT SELECT ON payments to owner1@localhost;
+GRANT SELECT ON transactions to owner1@localhost;
+GRANT SELECT ON sells to owner1@localhost;
+
+
+CREATE USER owner3@localhost IDENTIFIED by 'Prerak';
+GRANT SELECT ON owners_info to owner1@localhost;
+GRANT SELECT ON customers to owner1@localhost;
+GRANT DELETE ON accounts to owner1@localhost;
+GRANT SELECT ON accounts_info to owner1@localhost;
+GRANT SELECT ON sellers to owner1@localhost;
+GRANT SELECT ON buyers to owner1@localhost;
+GRANT ALL ON items to owner1@localhost;
+GRANT SELECT ON orders to owner1@localhost;
+GRANT SELECT ON ordered_items to owner1@localhost;
+GRANT SELECT ON support to owner1@localhost;
+GRANT SELECT, DELETE ON feedback to owner1@localhost;
+GRANT SELECT ON payments to owner1@localhost;
+GRANT SELECT ON transactions to owner1@localhost;
+GRANT SELECT ON sells to owner1@localhost;
+
+
+CREATE USER owner4@localhost IDENTIFIED by 'Vineet';
+GRANT SELECT ON owners_info to owner1@localhost;
+GRANT SELECT ON customers to owner1@localhost;
+GRANT DELETE ON accounts to owner1@localhost;
+GRANT SELECT ON accounts_info to owner1@localhost;
+GRANT SELECT ON sellers to owner1@localhost;
+GRANT SELECT ON buyers to owner1@localhost;
+GRANT ALL ON items to owner1@localhost;
+GRANT SELECT ON orders to owner1@localhost;
+GRANT SELECT ON ordered_items to owner1@localhost;
+GRANT SELECT ON support to owner1@localhost;
+GRANT SELECT, DELETE ON feedback to owner1@localhost;
+GRANT SELECT ON payments to owner1@localhost;
+GRANT SELECT ON transactions to owner1@localhost;
+GRANT SELECT ON sells to owner1@localhost;
