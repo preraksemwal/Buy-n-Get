bed1cd4acc7db3f7a50d7eb71616b353db663e8f
Janhavi2002 <88672685+Janhavi2002@users.noreply.github.com>
2022-04-24 18:01:20 +0530

    Update UI.py

--- UI.py
+++ UI.py
@@ -1,7 +1,36 @@
 from tkinter import *
 import mysql.connector as myConnector
 
+def details_page():
+    
+    frame=Frame(window, width=450, height=600)
+    Title = Label(frame, text= "Enter Details",font=("Arial",15, "bold")).place(x=155, y=40)
+    
+    gender = Label(frame, 
+                  text = "Gender").place(x = 60,
+                                           y = 110)  
+    gender_input_area = Entry(frame,width = 30).place(x = 200,y = 110)
 
+    Phone = Label(frame, 
+                      text = "Phone No.").place(x = 60,
+                                               y = 180)  
+    phone_input_area = Entry(frame,width = 30).place(x = 200,y = 180)
+    Street_no =Label(frame, 
+                      text = "Street No.").place(x = 60,
+                                               y = 250)
+    Street_no_input_area = Entry(frame,width = 30).place(x = 200,y = 250)
+    Street_name =Label(frame, 
+                      text = "Street Name").place(x = 60,
+                                               y = 320)
+    Street_name_input_area = Entry(frame,width = 30).place(x = 200,y = 320)
+    Pincode = Label(frame, text = "Pincode").place(x=60,y=390)
+    pincode_input_area = Entry(frame,width = 30).place(x = 200,y = 390)
+    state = Label(frame, text = "State").place(x=60,y=460)
+    state_input_area = Entry(frame,width = 30).place(x = 200,y = 460)
+    country = Label(frame, text = "Country").place(x=60,y=530)
+    country_input_area = Entry(frame,width = 30).place(x = 200,y = 530)
+    
+    frame.pack()
 
 def signup_page():
      
@@ -11,15 +40,16 @@ def signup_page():
     user_name     = Label(frame, text="Name").place(x=80, y=200)  
     user_email    = Label(frame, text="Email ID").place(x=80, y=270)  
     user_password = Label(frame, text=" New Password").place(x=80, y=340)
-    bday          = Label(frame, text="Birthday").place(x=80, y=410) 
+    age =Label(frame,text = "Age").place(x = 80,y = 410) 
     
-    submit_button = Button(frame, text="Next", height=1, width=8).place(x=200, y=470)
+    next_button = Button(frame, text = "Next",height= 1, width=8,command=lambda:[details_page(),frame.pack_forget()]).place(x = 200,
+                                              y = 470)
     back_button   = Button(frame, text="Back", height=1, width=8, command=frame.pack_forget).place(x=10, y=10)
     
     user_name_input_area     = Entry(frame, width=30).place(x=200, y=200)  
     user_email_input_area    = Entry(frame, width=30).place(x=200, y=270) 
     user_password_input_area = Entry(frame, width=30).place(x=200, y=340)
-    user_bday                = Entry(frame, width=30).place(x=200, y=410) 
+    user_age                = Entry(frame, width=30).place(x=200, y=410) 
     
     frame.pack()
     
