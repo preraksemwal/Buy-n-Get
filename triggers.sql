e22cd32c1c164b08ed5919ea653d51b10bbd3f76
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-04-24 12:41:30 +0530

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -25,3 +25,21 @@ CREATE TRIGGER stock_updation AFTER INSERT ON ordered_items
    
    END;//
 delimiter ;
+
+
+
+# if new rating entry inserted is less than the average, automatically issue a support entry
+delimiter //
+CREATE TRIGGER low_rating AFTER INSERT ON feedback
+   FOR EACH ROW
+   BEGIN
+   DECLARE new_rating int;
+   DECLARE Cid int;
+   SET new_rating = NEW.rating;
+   SET Cid = NEW.customer_id;
+   
+		IF new_rating < (SELECT AVG(rating) FROM feedback) THEN
+			INSERT INTO support (customer_id, issue, issue_date) values (Cid, 'Not Happy with Buynget Services', current_date());
+		END IF;
+   END;//
+delimiter ;
