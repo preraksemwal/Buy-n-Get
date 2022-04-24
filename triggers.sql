0310cadcb444d21a06ba9a1cad1fcdc86867957f
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-04-24 11:53:30 +0530

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -8,3 +8,20 @@ CREATE TRIGGER insertion BEFORE INSERT ON customers
 	   END IF;
    END;//
 delimiter ;
+
+
+
+# after each order, update the stock
+delimiter //
+CREATE TRIGGER stock_updation AFTER INSERT ON ordered_items
+   FOR EACH ROW
+   BEGIN
+   DECLARE id int;
+   DECLARE q int;
+   SET id = NEW.item_id;	
+   SET q = NEW.quantity;
+   
+   UPDATE items SET items.quantity = items.quantity - q where item_id = id;
+   
+   END;//
+delimiter ;
