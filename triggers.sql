a41a15b6e34a24ec6ae38a57f4083c4e255e2fbf
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-04-24 11:25:35 +0530

    Update triggers.sql

--- triggers.sql
+++ triggers.sql
@@ -1,1 +1,10 @@
-
+# if age is NULL replace with 18
+delimiter //
+CREATE TRIGGER insertion BEFORE INSERT ON customers
+   FOR EACH ROW
+   BEGIN
+	   IF NEW.age IS NULL THEN
+		   SET NEW.age = 18;
+	   END IF;
+   END;//
+delimiter ;
