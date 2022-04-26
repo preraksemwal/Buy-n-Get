bd8608a7f0c2d6a694743168ec079133d33e41e0
The Punisher <88607607+VineetVK09@users.noreply.github.com>
2022-04-26 11:42:47 +0530

    Create Vineet.py

---
+++ Vineet.py
@@ -0,0 +1,20 @@
+# 7  :  name input  (details page)
+# 8  :  age input  (details page)
+# 9  :  gender input  (details page)
+# 10 :  phone input  (details page)
+# 11 :  country input  (details page)
+# 12 :  state input  (details page)
+# 13 :  street name input  (details page)
+# 14 :  street no input  (details page)
+# 15 :  street pincode input  (details page)
+
+
+details_name_input           = StringVar();    variables.append(details_name_input);
+details_age_input            = IntVar();       variable.append(details_age_input);
+details_gender_input         = StringVar();    variable.append(details_gender_input);
+details_phone_input          = StringVar();    variable.append(details_phone_input);
+details_country_input        = StringVar();    variable.append(details_country_input);
+details_state_input          = StringVar();    variable.append(details_state_input);
+details_streetname_input     = StringVar();    variable.append(details_streetname_input);
+details_streetno_input       = IntVar();       variable.append(details_streetno_input);
+details_pincode_input        = StringVar();    variable.append(details_pincode_input);
