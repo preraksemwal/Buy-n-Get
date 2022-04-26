a2d8cd6a7597c9e5e59087cad44d744b62a62e6c
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-04-26 10:37:22 +0530

    Update populate_all.sql

--- populate_all.sql
+++ populate_all.sql
@@ -115,7 +115,30 @@ insert into items (item_name, cost_price, selling_price, quantity, item_type) va
 
 
 
-
+insert into support (support_id, customer_id, issue,issue_date) values(1, 269, 'Ihe colour of phone does not match with the one I ordered', '2021-10-10');
+insert into support (customer_id, issue, issue_date) values( 450, 'I am not able to add products to my cart', '2022-11-12');
+insert into support (customer_id, issue, issue_date) values( 73, 'The iron I ordered is not heating up properly', '2022-01-27');
+insert into support (customer_id, issue, issue_date) values( 90, 'The lid of the blender was broken', '2022-02-19');
+insert into support (customer_id, issue, issue_date) values( 90, 'The lid of the blender was broken', '2022-02-27');
+insert into support (customer_id, issue, issue_date) values( 123, 'Laptop is not turning on', '2022-02-25');
+insert into support (customer_id, issue, issue_date) values( 234, 'Earphone not working', '2022-02-23');
+insert into support (customer_id, issue, issue_date) values( 345, 'I am not able to place order', '2022-02-23');
+insert into support (customer_id, issue, issue_date) values( 456, 'I have not recieved my order which was meant to be delivered yesterday', '2022-02-27');
+insert into support (customer_id, issue, issue_date) values( 678, 'Wrong item was sent', '2022-02-25');
+insert into support (customer_id, issue, issue_date) values( 890, 'Quality not adequate', '2022-02-18');
+insert into support (customer_id, issue, issue_date) values( 901, 'The packaging of the product was already opened.', '2022-02-15');
+insert into support (customer_id, issue, issue_date) values( 433, 'Unavailable or out of stock product', '2022-02-10');
+insert into support (customer_id, issue, issue_date) values( 167, 'Money not refunded', '2022-02-5');
+insert into support (customer_id, issue, issue_date) values( 298, 'The toothpaste was expired', '2022-01-26');
+insert into support (customer_id, issue, issue_date) values( 888, 'Billing Issues', '2022-01-23');
+insert into support (customer_id, issue, issue_date) values( 40, 'The fruits were not fresh', '2022-01-20');
+insert into support (customer_id, issue, issue_date) values( 567, 'I am unable to avail the displayed offer', '2022-01-19');
+insert into support (customer_id, issue, issue_date) values( 786, 'The lid of the blender was broken', '2022-01-19');
+insert into support (customer_id, issue, issue_date) values( 99, 'I am unable to avail the displayed offer', '2022-01-13');
+insert into support (customer_id, issue, issue_date) values( 200, 'Unavailable or out of stock product', '2022-01-13');
+insert into support (customer_id, issue, issue_date) values( 100, 'I am unable to avail the displayed offer', '2022-01-10');
+insert into support (customer_id, issue, issue_date) values( 300, 'unable to log in on the app', '2022-01-10');
+insert into support (customer_id, issue, issue_date) values( 444, 'Prices Are Too High', '2020-01-10');
 
 insert into feedback values(23, 4);
 insert into feedback values(1, 5);
@@ -158,31 +181,6 @@ insert into feedback values(399, 3);
 insert into feedback values(666, 3);
 insert into feedback values(786, 5);
 insert into feedback values(903, 5);
-
-insert into support (support_id, customer_id, issue,issue_date) values(1, 269, 'Ihe colour of phone does not match with the one I ordered', '2021-10-10');
-insert into support (customer_id, issue, issue_date) values( 450, 'I am not able to add products to my cart', '2022-11-12');
-insert into support (customer_id, issue, issue_date) values( 73, 'The iron I ordered is not heating up properly', '2022-01-27');
-insert into support (customer_id, issue, issue_date) values( 90, 'The lid of the blender was broken', '2022-02-19');
-insert into support (customer_id, issue, issue_date) values( 90, 'The lid of the blender was broken', '2022-02-27');
-insert into support (customer_id, issue, issue_date) values( 123, 'Laptop is not turning on', '2022-02-25');
-insert into support (customer_id, issue, issue_date) values( 234, 'Earphone not working', '2022-02-23');
-insert into support (customer_id, issue, issue_date) values( 345, 'I am not able to place order', '2022-02-23');
-insert into support (customer_id, issue, issue_date) values( 456, 'I have not recieved my order which was meant to be delivered yesterday', '2022-02-27');
-insert into support (customer_id, issue, issue_date) values( 678, 'Wrong item was sent', '2022-02-25');
-insert into support (customer_id, issue, issue_date) values( 890, 'Quality not adequate', '2022-02-18');
-insert into support (customer_id, issue, issue_date) values( 901, 'The packaging of the product was already opened.', '2022-02-15');
-insert into support (customer_id, issue, issue_date) values( 433, 'Unavailable or out of stock product', '2022-02-10');
-insert into support (customer_id, issue, issue_date) values( 167, 'Money not refunded', '2022-02-5');
-insert into support (customer_id, issue, issue_date) values( 298, 'The toothpaste was expired', '2022-01-26');
-insert into support (customer_id, issue, issue_date) values( 888, 'Billing Issues', '2022-01-23');
-insert into support (customer_id, issue, issue_date) values( 40, 'The fruits were not fresh', '2022-01-20');
-insert into support (customer_id, issue, issue_date) values( 567, 'I am unable to avail the displayed offer', '2022-01-19');
-insert into support (customer_id, issue, issue_date) values( 786, 'The lid of the blender was broken', '2022-01-19');
-insert into support (customer_id, issue, issue_date) values( 99, 'I am unable to avail the displayed offer', '2022-01-13');
-insert into support (customer_id, issue, issue_date) values( 200, 'Unavailable or out of stock product', '2022-01-13');
-insert into support (customer_id, issue, issue_date) values( 100, 'I am unable to avail the displayed offer', '2022-01-10');
-insert into support (customer_id, issue, issue_date) values( 300, 'unable to log in on the app', '2022-01-10');
-insert into support (customer_id, issue, issue_date) values( 444, 'Prices Are Too High', '2020-01-10');
 
 insert into payments(payment_id, customer_id, mode, amount) values (1, 2, 'NetBanking', 100);
 insert into payments(customer_id, mode, amount) values (2, 'NetBanking', 37.4);
