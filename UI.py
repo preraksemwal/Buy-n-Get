4b0d0b1ff5bc81b258149615d2112d2c7eb4f192
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-04-26 18:43:21 +0530

    Update UI.py

--- UI.py
+++ UI.py
@@ -18,7 +18,8 @@ def login():
     try:
         count = count[0]
         if count[0] == username and count[2] == (password):
-            print("Welcome", username, "!")
+            MessageBox.showinfo( "Logged In", "Welcome " + username + " !")
+            # print("Welcome", username, "!")
             variables[2].pack_forget()
             category()
     except:
