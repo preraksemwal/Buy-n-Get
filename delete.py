cb34c2cba54fd6a22cd23036d753b086b5199664
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-04-27 10:55:39 +0530

    Update and rename Vineet.py to delete.py

---
+++ delete.py
@@ -0,0 +1,10 @@
+use buynget; 
+
+drop user owner@localhost;
+drop user seller@localhost;
+drop user buyer@localhost;
+
+drop trigger insertion;
+drop trigger stock_updation;
+drop trigger low_rating;
+drop trigger transaction_completed;
