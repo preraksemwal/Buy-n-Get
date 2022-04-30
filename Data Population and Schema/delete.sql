12664cb5be32b0c701b4f22ac05e58aeee71530c
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-05-01 01:26:13 +0530

    Update delete.sql

--- Data Population and Schema/delete.sql
+++ Data Population and Schema/delete.sql
@@ -1,19 +1,25 @@
+
 use buynget;
 
 drop view owners_info;
 drop view accounts_info;
 drop view customers_info;
+drop view items_info;
 
 drop user owner1@localhost;
 drop user owner2@localhost;
 drop user owner3@localhost;
 drop user owner4@localhost;
-drop user seller@localhost;
-drop user buyer@localhost;
+drop user seller1@localhost;
+drop user seller2@localhost;
+drop user buyer1@localhost;
+drop user buyer2@localhost;
 
 drop role team;
+drop role buy;
+drop role sell;
 
 drop trigger insertion;
 drop trigger stock_updation;
 drop trigger low_rating;
-drop trigger order_completed ;
+drop trigger order_completed;
