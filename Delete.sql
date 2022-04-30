517e57e452fdf8cac7fd1ea4c95cdaef13058764
Prerak Semwal <77500750+preraksemwal@users.noreply.github.com>
2022-04-30 17:39:43 +0530

    Update Delete.sql

--- Delete.sql
+++ Delete.sql
@@ -1,10 +1,17 @@
-use buynget; 
+drop view owners_info;
+drop view accounts_info;
+drop view customers_view;
 
-drop user owner@localhost;
+drop user owner1@localhost;
+drop user owner2@localhost;
+drop user owner3@localhost;
+drop user owner4@localhost;
 drop user seller@localhost;
 drop user buyer@localhost;
+
+drop role owner_;
 
 drop trigger insertion;
 drop trigger stock_updation;
 drop trigger low_rating;
-drop trigger transaction_completed;
+drop trigger order_completed ;
